860bed4e372290cefa0416a87f665ebbd6b52904
Vicent Niad <glory.jly@gmail.com>
2021-01-18 18:08:33 +0300

    fix : try to add DB, but failed

--- src/ID/ID/Controllers/Registration.cs
+++ src/ID/ID/Controllers/Registration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ID.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,12 @@ namespace ID.Controllers
         [HttpPost]
         public void Index(string nikname, string mail)
         {
+            User user = new User
+            {
+                Name = nikname,
+                Mail = mail,
+            };
+
         }
 
     }
